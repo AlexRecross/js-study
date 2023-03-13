4166576a27d195502c87338a9201c166c9873133
AlexRecross <perekhrestalexandr@gmail.com>
2023-03-13 18:23:11 +0200

    feature/L8 - process point - improvements for helper - improvements for task 2 - implemented task 5+6+7+8

--- L8/helper.js
+++ L8/helper.js
@@ -2,10 +2,10 @@ function genInt (from = 0, to = 100) {
 	return Math.floor(Math.random() * (from - to) + to);
 }
 
-function genArr (lenght = 10) {
+function genArr (lenght = 10, min, max) {
 	var result = [];
 	while (lenght) {
-		var randNum = genInt();
+		var randNum = genInt(min, max);
 		result.push(randNum);
 		--lenght;
 	} 
