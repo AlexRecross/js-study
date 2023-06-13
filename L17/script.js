331241335bd01d225d1b69a53f9bd6cbb6b5f667
AlexRecross <perekhrestalexandr@gmail.com>
2023-06-13 11:06:29 +0300

    Featured
    L17
    
    add terms&condition template with validatoin
    
    custom focus border
    
    cleaning

--- L17/script.js
+++ L17/script.js
@@ -1,119 +1,115 @@
-var carForm = document.forms.carForm;
-var carBrand = carForm.brand;
-var carModel = carForm.model;
-var userName = carForm.UserName;
-var userEmail = carForm.UserEmail;
+document.addEventListener('DOMContentLoaded', function () {
+	
+	var carForm = document.forms.carForm;
+	var carBrand = carForm.brand;
+	var carModel = carForm.model;
+	var userName = carForm.UserName;
+	var userEmail = carForm.UserEmail;
 
 
-// input text interaction
-// user Name
-var userNamePlaceholder = userName.placeholder;
+	// input text interaction
+	// user Name
+	var userNamePlaceholder = userName.placeholder;
 
-userName.addEventListener("focus", function() {
-	userName.placeholder = "";
-});
+	userName.addEventListener("focus", function() {
+		userName.placeholder = "";
+	});
 
-carBrand.addEventListener("blur", function() {
-	userName.placeholder = userNamePlaceholder;
-});
+	carBrand.addEventListener("blur", function() {
+		userName.placeholder = userNamePlaceholder;
+	});
 
-// UserEmail
+	// UserEmail
 
-var userEmailPlaceholder = userEmail.placeholder;
+	var userEmailPlaceholder = userEmail.placeholder;
 
-userEmail.addEventListener("focus", function() {
-	userEmail.placeholder = "";
-});
+	userEmail.addEventListener("focus", function() {
+		userEmail.placeholder = "";
+	});
 
-userEmail.addEventListener("blur", function() {
-	userEmail.placeholder = userEmailPlaceholder;
-});
+	userEmail.addEventListener("blur", function() {
+		userEmail.placeholder = userEmailPlaceholder;
+	});
 
-// cars brand
+	// cars brand
 
-var carBrandPlaceholder = carBrand.placeholder;
+	var carBrandPlaceholder = carBrand.placeholder;
 
-carBrand.addEventListener("focus", function() {
-	carBrand.placeholder = "";
-});
+	carBrand.addEventListener("focus", function() {
+		carBrand.placeholder = "";
+	});
 
-carBrand.addEventListener("blur", function() {
-	carBrand.placeholder = carBrandPlaceholder;
-});
+	carBrand.addEventListener("blur", function() {
+		carBrand.placeholder = carBrandPlaceholder;
+	});
 
-// cars model
+	// cars model
 
-var carModelPlaceholder = carModel.placeholder;
+	var carModelPlaceholder = carModel.placeholder;
 
-carModel.addEventListener("focus", function() {
-	carModel.placeholder = "";
-});
+	carModel.addEventListener("focus", function() {
+		carModel.placeholder = "";
+	});
 
-carModel.addEventListener("blur", function(){
-	carModel.placeholder = carModelPlaceholder;
-});
+	carModel.addEventListener("blur", function(){
+		carModel.placeholder = carModelPlaceholder;
+	});
 
 
 
-// validation
-carForm.addEventListener('submit', function() {
-	event.preventDefault();
+	// validation
+	carForm.addEventListener('submit', function() {
+		event.preventDefault();
 
-	var error = formValidate(carForm);
+		var error = formValidate(carForm);
 
-	if (error === 0) {
-		carForm.classList.add('sending');
-	} else {
-		console.log('Fill in required fields');
-	}
-});
-
-function formValidate(form) {
-	var error = 0;
-	var formReq =  carForm.getElementsByClassName('required')
-
-	for (var i = 0; i < formReq.length; i++) {
-		var input = formReq[i];
-		formRemoveError(input);
-
-		if (input.value =='') {
-			formAddError(input);
-			error++;
+		if (error === 0) {
+			carForm.classList.add('sending');
+		} else {
+			console.log('Fill in required fields');
 		}
-		if (input.classList.contains('email')) {
-			if (emailTest(input)) {
+	});
+
+	function formValidate(form) {
+		var error = 0;
+		var formReq =  carForm.getElementsByClassName('required')
+
+		for (var i = 0; i < formReq.length; i++) {
+			var input = formReq[i];
+			formRemoveError(input);
+
+			if (input.value =='') {
 				formAddError(input);
 				error++;
 			}
+
+			if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+				formAddError(input);
+				error++;
+			}
+
+			if (input.classList.contains('email')) {
+				if (emailTest(input)) {
+					formAddError(input);
+					error++;
+				}
+			}
 		}
+		return error;
 	}
-	return error;
-}
 
-function formAddError(input) {
-	input.parentElement.classList.add('error');
-	input.classList.add('error');
-}
+	function formAddError(input) {
+		input.parentElement.classList.add('error');
+		input.classList.add('error');
+	}
 
-function formRemoveError(input) {
-	input.parentElement.classList.remove('error');
-	input.classList.remove('error');
-}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('error');
+		input.classList.remove('error');
+	}
 
 
-function emailTest(input) {
-	return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-}
-
-// carForm.addEventListener("submit", function() {
-// 	console.log('Form sending...');
-
-// 	if(!carBrand.value) {
-// 		console.log("Sending canceled. Car's brand is empty")
-// 		event.preventDefault();
-// 	}
-// 	if(!carModel.value) {
-// 		console.log("Sending canceled. Car's model is empty")
-// 		event.preventDefault();
-// 	}
-// })
+	function emailTest(input) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+});
